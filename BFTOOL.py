cecfaa359b3aa5dadefb71432ac793694f903d0e
Hani Khader <htkhader@gmail.com>
2023-04-08 13:42:17 +0200

    Reading txt lines in an array

--- BFTOOL.py
+++ BFTOOL.py
@@ -4,12 +4,22 @@ import mmh3
 import bitarray
 
 # Open the text file containing the list of passwords
-with open('passwords.txt', 'r') as f:
-    passwords = f.read().splitlines()
+passwords = []
+with open('pwnedpasswords.txt', 'r') as f:
+    for line in f:
+        passwords.append(line.strip())
 
-# Define the size of the Bloom filter and the number of hash functions to use
-num_bits = 1000000
-num_hashes = 5
+# If the file is small, open it as a whole
+#with open('pwnedpasswords.txt', 'r') as f:
+#    passwords = f.read().splitlines()
+
+# Define the desired false positive rate
+false_positive_rate = 0.0000001
+
+# Calculate the required size of the bit array and the number of hash functions
+#num_bits = 34359738368 # 4 Gb
+num_bits = int(-(len(passwords) * math.log(false_positive_rate)) / (math.log(2) ** 2))
+num_hashes = int((num_bits / len(passwords)) * math.log(2))
 
 # Create a bit array of the specified size, initialized to all 0s
 bit_array = bitarray.bitarray(num_bits)
