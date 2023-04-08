ac7caaa844d7e250483baa6c39e9b0f68f4a01a3
Hani Khader <htkhader@gmail.com>
2023-04-08 14:14:08 +0200

    input: lowering strain on ram

--- BFTOOL.py
+++ BFTOOL.py
@@ -12,6 +12,9 @@ import resource
 #memory_limit_bytes = 5 * 1024 * 1024 * 1024
 #resource.setrlimit(resource.RLIMIT_AS, (memory_limit_bytes,memory_limit_bytes))
 
+# Add number of lines extracter using wc -l {name of the file}
+# grip the numbers value and put it in a variable
+
 def format_duration(duration):
     seconds = duration #// 1000000
     hours, remainder = divmod(seconds, 3600)
@@ -45,43 +48,41 @@ while True:
 
 # Logging
 start_time = time.perf_counter()
-now = datetime.datetime.now()
 time_str0 = now.strftime("%H:%M:%S.%f")[:-1]
 date_str = now.strftime("%Y%m%d")
 logfile = f"BfG_Log_{date_str}.log"
 logging.basicConfig(filename=logfile, level=logging.INFO)
 
-
-# Open the text file containing the list of passwords
-passwords = []
-with open('pwnedpasswords.txt', 'r') as f:
-    for line in f:
-        passwords.append(line.strip())
-
-# If the file is small, open it as a whole
-#with open('pwnedpasswords.txt', 'r') as f:
-#    passwords = f.read().splitlines()
-
 # Define the desired false positive rate
-false_positive_rate = 0.0000001
+false_positive_rate = 0.001
+
+#number_of_lines = 851082816
+#number_of_lines = 39
 
 # Calculate the required size of the bit array and the number of hash functions
 #num_bits = 34359738368 # 4 Gb
-num_bits = int(-(len(passwords) * math.log(false_positive_rate)) / (math.log(2) ** 2))
-num_hashes = int((num_bits / len(passwords)) * math.log(2))
+num_bits = int(-(number_of_lines * math.log(false_positive_rate)) / (math.log(2) ** 2))
+num_hashes = int((num_bits / number_of_lines) * math.log(2))
+#num_bits = int(math.ceil(num_bits_i / num_hashes) * num_hashes)
+
 
 # Create a bit array of the specified size, initialized to all 0s
 bit_array = bitarray.bitarray(num_bits)
 bit_array.setall(False)
 
 # For each password in the list, generate num_hashes hash values using MurmurHash3 and set the corresponding bits in the bit array to 1
-for password in passwords:
-    for i in range(num_hashes):
-        hash_value = mmh3.hash64(password.encode(), i)[0] % num_bits
-        bit_array[hash_value] = True
+# Open the text file containing the list of passwords
+#with open('pwnedpasswords.txt', 'r') as f:
+with open(textFile, 'r') as f:
+    for line in f:
+        password = line.strip()
+        for i in range(num_hashes):
+            hash_value = mmh3.hash64(password.encode(), i)[0] % num_bits
+            bit_array[hash_value] = True
 
 # Write the Bloom filter to a binary file
-with open('finalfinalBF.bin', 'wb') as f:
+#with open('finalfinalBF.bin', 'wb') as f:
+with open(binFile, 'wb') as f:
     bit_array.tofile(f)
 
 end_time = time.perf_counter()
