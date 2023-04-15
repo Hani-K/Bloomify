f10a69c6652ffb261fd47055408f385db8bf6873
Hani K <htkhader@gmail.com>
2023-04-15 15:48:25 +0200

    BFTOOL Banner

--- BFTOOL.py
+++ BFTOOL.py
@@ -16,12 +16,21 @@ import wCount
 import optimalP
 import readline
 import bfg
+from color import Color
 
 readline.parse_and_bind('tab: complete')
 
+def print_banner():
+    Color.pl(r'{R}     ____  ________________  ____  __  {R}')
+    Color.pl(r'{R}    / __ )/ ____/_  __/ __ {O}\/ {R}__ {O}\/ {R}/  {G}BFTOOL {W}0.4')
+    Color.pl(r'{R}   / __  / /_    / / / / / / / / / /   {W}{D}a list manuplation and bloom filter generation tool{R}')
+    Color.pl(r'{R}  / /_/ / __/   / / / /_/ / /_/ / /___ {W}{D}created and maintained by Hani K.{R}')
+    Color.pl(r'{R} /_____/_/     /_/  {W}{O}\_{R}___/{O}\_{R}___/_____/ {C}{D}https://github.com/Hani-K/bftool{W}')
+    Color.pl('\n')
 
-    
+
 if __name__ == '__main__':
+    print_banner()
     logger.startLogging()
     bfg.bfg()
     logger.endLogging()
